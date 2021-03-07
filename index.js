cf7633921af33debeea5bea5ee2ca61696f851d2
ak-matsu <sandbox.matsu@gmail.com>
2021-03-07 21:20:54 +0900

    Window表示時にJavaScriptが実行されるように設定

---
+++ index.js
@@ -0,0 +1,10 @@
+
+// ページ全体が読み込まれたあとにJavaScriptの処理が実行される。
+window.addEventListener('load',function(){
+// constは後から書き換えることのできない変数
+// letは後から書き換えることのできる変数
+// DOMはDocument,Object,model
+// getElementById("id名")はDOMツリーから特定のHTML要素を取得するためのメソッドの1つ。
+// pullDownButton変数へlistsを引き渡す。
+const pullDownButton = document.getElementById("lists")
+})
